b862410b8f63fe96a8615ef94e6613a5ac20b996
arafatgw5-cpu <arafatgw5@gmail.com>
2026-03-08 13:14:49 +0600

    commit 6

--- script.js
+++ script.js
@@ -16,3 +16,24 @@ alert("Invalid credentials")
 }
 
 }
+const API="https://phi-lab-server.vercel.app/api/v1/lab/issues"
+
+const container=document.getElementById("issuesContainer")
+
+const loader=document.getElementById("loader")
+
+// LOAD ALL
+
+async function loadAll(){
+
+loader.classList.remove("hidden")
+
+const res=await fetch(API)
+
+const data=await res.json()
+
+displayIssues(data.data)
+
+loader.classList.add("hidden")
+
+}
